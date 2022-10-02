123f10b0f5386a6c3e863a6bc1202231661ae06e
Koen Klaren <koen.klaren@gmail.com>
2022-10-02 12:36:57 +0200

    Analyze single liberties (and solve first puzzle!)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use colored::*;
-use std::{cell::RefCell, fmt::Display, rc::Rc, collections::HashSet};
+use std::{cell::RefCell, collections::HashSet, fmt::Display, rc::Rc};
 
 type Coord = (usize, usize);
 
@@ -45,6 +45,7 @@ struct Cell {
     region: Option<Rc<RefCell<Region>>>,
 }
 
+#[derive(Debug)]
 struct Region {
     state: State,
     /// Coordinates of cells in the region
@@ -58,6 +59,7 @@ struct Grid {
     height: usize,
     cells: Vec<Cell>,
     regions: Vec<Rc<RefCell<Region>>>,
+    total_black_cells: usize,
 }
 
 impl Grid {
@@ -71,6 +73,8 @@ impl Grid {
         ];
         let mut regions = vec![];
 
+        let mut total_white_cells = 0;
+
         for &(coord, given) in &givens {
             let state = State::Numbered(given);
             let region_ptr = Rc::new(RefCell::new(Region {
@@ -83,6 +87,8 @@ impl Grid {
                 state: State::Numbered(given),
                 region: Some(region_ptr.clone()),
             };
+
+            total_white_cells += given;
         }
 
         Grid {
@@ -90,6 +96,7 @@ impl Grid {
             height,
             cells,
             regions,
+            total_black_cells: width * height - total_white_cells,
         }
     }
 
@@ -113,34 +120,68 @@ impl Grid {
         // TODO: Return Result:Err instead of panicking when contradiction occurs
         assert_eq!(self.cells[index].state, State::Unknown);
 
-        // Create new region containing only the given cell
-        let region = Rc::new(RefCell::new(Region {
-            state,
-            coords: vec![coord],
-            unknowns: self.valid_unknown_neighbors(coord),
-        }));
-        self.regions.push(region.clone());
+        {
+            // Create new region containing only the given cell
+            let region = Rc::new(RefCell::new(Region {
+                state,
+                coords: vec![coord],
+                unknowns: self.valid_unknown_neighbors(coord),
+            }));
+            self.regions.push(region.clone());
 
-        // Mark the given cell, and link it to the new region
-        self.cells[index].state = state;
-        self.cells[index].region = Some(region.clone());
+            // Mark the given cell, and link it to the new region
+            self.cells[index].state = state;
+            self.cells[index].region = Some(region.clone());
+        }
 
         // Update adjacent regions
         for adjacent_coord in self.valid_neighbors(coord) {
             let adjacent_index = self.coord_to_index(adjacent_coord);
-            let adjacent_cell = &self.cells[adjacent_index];
 
-            if let Some(adjacent_region_ptr) = &adjacent_cell.region {
-                let mut adjacent_region = adjacent_region_ptr.borrow_mut();
-
+            if let Some(adjacent_region_ptr) = self.cells[adjacent_index].region.clone() {
                 // Remove cell from the unknowns of all adjacent regions
                 // TODO: Performance can probably be improved slightly by using .swap_remove()
-                adjacent_region.unknowns.retain(|&unknown| unknown != coord);
+                adjacent_region_ptr.borrow_mut().unknowns.retain(|&unknown| unknown != coord);
 
-                // TODO: Add cell to adjacent regions with equivalent state, potentially fusing some regions
+                // Add cell to adjacent regions with equivalent state, potentially fusing some regions
+                // TODO: Make sure that this equivalence check is correct
+                let is_adjacent_state_equivalent = match adjacent_region_ptr.borrow().state {
+                    State::Unknown => unreachable!(),
+                    State::White | State::Numbered(_) => state == State::White,
+                    State::Black => state == State::Black,
+                };
+                if is_adjacent_state_equivalent {
+                    self.fuse_regions(adjacent_region_ptr, self.cells[index].region.clone().unwrap());
+                }
             }
-
         }
+    }
+
+    fn fuse_regions(&mut self, r1: Rc<RefCell<Region>>, r2: Rc<RefCell<Region>>) {
+        // TODO: Check correctness. E.g. currently we might lose a numbered region if r1 is white and r2 is numbered.
+
+        // No need to fuse a region to itself
+        if Rc::ptr_eq(&r1, &r2) {
+            return;
+        }
+
+        // Add cells of r2 to r1
+        r1.borrow_mut().coords.extend(r2.borrow().coords.iter());
+        for &coord in &r2.borrow().coords {
+            let index = self.coord_to_index(coord);
+            self.cells[index].region = Some(r1.clone());
+        }
+
+        // Add new unknowns from r2 to r1
+        for &coord in &r2.borrow().unknowns {
+            if !r1.borrow().unknowns.contains(&coord) {
+                r1.borrow_mut().unknowns.push(coord);
+            }
+        }
+
+        // Remove r2 from the grid
+        self.regions.retain(|r| !Rc::ptr_eq(r, &r2));
+        assert_eq!(Rc::strong_count(&r2), 1);
     }
 
     fn analyze_complete_islands(&mut self) {
@@ -157,6 +198,38 @@ impl Grid {
 
         for coord in mark_as_black {
             self.mark_cell(coord, State::Black);
+        }
+    }
+
+    fn analyze_single_liberties(&mut self) {
+        let mut mark_as_black = HashSet::<Coord>::new();
+        let mut mark_as_white = HashSet::<Coord>::new();
+
+        for region_ptr in &self.regions {
+            let region = region_ptr.borrow();
+
+            let is_region_complete = match region.state {
+                State::Unknown => unreachable!(),
+                State::White => false,
+                State::Black => region.coords.len() == self.total_black_cells,
+                State::Numbered(number) => region.coords.len() == number,
+            };
+
+            if !is_region_complete && region.unknowns.len() == 1 {
+                let unknown_coord = region.unknowns[0];
+                match region.state {
+                    State::Unknown => unreachable!(),
+                    State::White | State::Numbered(_) => mark_as_white.insert(unknown_coord),
+                    State::Black => mark_as_black.insert(unknown_coord),
+                };
+            }
+        }
+
+        for coord in mark_as_black {
+            self.mark_cell(coord, State::Black);
+        }
+        for coord in mark_as_white {
+            self.mark_cell(coord, State::White);
         }
     }
 }
@@ -199,5 +272,37 @@ mod tests {
 
         grid.analyze_complete_islands();
         println!("{}", grid);
+
+        grid.analyze_single_liberties();
+        println!("{}", grid);
+
+        grid.analyze_single_liberties();
+        println!("{}", grid);
+
+        grid.analyze_complete_islands();
+        println!("{}", grid);
+
+        grid.analyze_single_liberties();
+        println!("{}", grid);
+
+        grid.analyze_single_liberties();
+        println!("{}", grid);
+
+        grid.analyze_single_liberties();
+        println!("{}", grid);
+
+        grid.analyze_single_liberties();
+        println!("{}", grid);
+
+        grid.analyze_single_liberties();
+        println!("{}", grid);
+
+        grid.analyze_complete_islands();
+        println!("{}", grid);
+
+        grid.analyze_single_liberties();
+        println!("{}", grid);
+
+        println!("{:#?}", grid.regions);
     }
 }
