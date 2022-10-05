13f908a7693e61446ea770f4eed2d5a1a53395c7
Koen Klaren <koen.klaren@gmail.com>
2022-10-05 16:59:47 +0200

    Add larger example puzzle

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,11 @@ fn main() {
 
     // https://www.puzzle-nurikabe.com/
     // 5x5 Nurikabe Hard Puzzle ID: 9,690,008
-    let mut grid = Grid::from_str("2..1./...../...3./....5/.....").unwrap();
+    //let mut grid = Grid::from_str("2..1./...../...3./....5/.....").unwrap();
+
+    // https://en.wikipedia.org/wiki/Nurikabe_(puzzle)
+    let mut grid = Grid::from_str("2........2/......2.../.2..7...../........../......3.3./..2....3../2..4....../........../.1....2.4.").unwrap();
+
     println!("{}", grid);
     solver.solve(&mut grid);
 }
